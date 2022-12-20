5dd0a483424305651a4c91e7cd5f9f9ef633a763
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-12-19 22:25:15 -0300

    dart - estruturas de controle

---
+++ curso_dart/controle/desafio_if_else.dart
@@ -0,0 +1,19 @@
+main() {
+  var nota = 0.3;
+
+  if (nota >= 9.0) // ; NÃO USA ; EM ESTRUTURAS DE CONTROLE *
+  {
+    print('Parabéns! Vc foi brilhante!');
+  }
+
+  // * EXCEÇÃO DO/WHILE!!!
+
+  if (nota >= 9.0) 
+  {
+    
+  }
+
+  {
+    print('Parabéns! Vc foi brilhante!');
+  }
+}
