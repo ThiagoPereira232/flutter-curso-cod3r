5616430df6b076ed8506784101d84cab2147cac0
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-12-26 14:53:19 -0300

    refatoração

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:expenses/components/transaction_list.dart';
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(const ExpensesApp());
@@ -9,32 +8,14 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
-
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  final _transaction = [
-    Transaction(
-      id: 't1',
-      title: 'novo tenis',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'conta de luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -44,49 +25,13 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Card(
+        children: const [
+          Card(
             color: Colors.blue,
             elevation: 5,
             child: Text("Grafico"),
           ),
-          TransactionList(_transaction),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: const InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: const InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          print(titleController.text);
-                          print(valueController.text);
-                        },
-                        style: TextButton.styleFrom(
-                            foregroundColor: Colors.purple),
-                        child: const Text('Nova Transação'),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )
+          TransactionUser(),
         ],
       ),
     );
