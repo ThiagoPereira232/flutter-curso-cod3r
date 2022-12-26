7d92d7f214bc60882503f5b1934af61d8d7c84a0
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-12-26 16:22:47 -0300

    modal

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -1,5 +1,10 @@
-import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
+
+import 'dart:math';
+
+import './components/transaction_form.dart';
+import './components/transaction_list.dart';
+import 'package:expenses/models/transaction.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -14,8 +19,52 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transaction = [
+    Transaction(
+      id: 't1',
+      title: 'novo tenis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transaction.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +73,7 @@ class MyHomePage extends StatelessWidget {
         title: const Text("Despesas Pessoais"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _openTransactionFormModal(context),
             icon: const Icon(Icons.add),
           ),
         ],
@@ -32,18 +81,18 @@ class MyHomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            Card(
+          children: [
+            const Card(
               color: Colors.blue,
               elevation: 5,
               child: Text("Grafico"),
             ),
-            TransactionUser(),
+            TransactionList(_transaction),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => _openTransactionFormModal(context),
         child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
