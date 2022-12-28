edb81cf19d09f604a3ec11f40333b5d682fa41b6
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-12-28 16:54:41 -0300

    listtile e chart

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -69,6 +69,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: 'conta de luz',
         value: 211.30,
         date: DateTime.now().subtract(Duration(days: 4))),
+    Transaction(
+      id: 't3',
+      title: 'dsa',
+      value: 211322.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'food',
+      value: 11.30,
+      date: DateTime.now(),
+    ),
   ];
 
   List<Transaction> get _recentTransactions {
